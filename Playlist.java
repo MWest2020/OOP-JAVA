a7ee0b82a14b6db64ad179f7eacabef2387afd9f
MWest2020 <mwesterweel@hotmail.com>
2022-02-06 21:48:25 +0100

    Playlist.java add for practicum 1

---
+++ Playlist.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Playlist {
+    public static void main(String [] args){
+        String firstName = "Mark";
+        String lastName = "Westerweel";
+        String studentNumber = "500836508";
+        
+        System.out.println("First name is: " + firstName);
+        System.out.println("Last name is: " + lastName);
+        System.out.println("Student number is: " + studentNumber);
+
+        Scanner 
+    }
+}
