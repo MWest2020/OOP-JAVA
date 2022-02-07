3277bb82028ff376368d7b8af9977ef9f8d5e0b7
MWest2020 <mwesterweel@hotmail.com>
2022-02-07 16:00:50 +0100

    user input for track title and duration

--- Playlist.java
+++ Playlist.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-import java.util.HashMap;
+import java.util.ArrayList;
+
 
 /*
 *     @author Mark Westerweel, Studentnumber 500836508
@@ -15,8 +16,7 @@ import java.util.HashMap;
 
 
 class Playlist {
-     
-     
+         
     public static void main(String[] args){
         String firstName = "Mark";
         String lastName = "Westerweel";
@@ -27,17 +27,39 @@ class Playlist {
         System.out.println("Student number is: " + studentNumber);
 
         int userTracklist;
+        int min = 2;
+        int max = 20;
+        
         do {
-            try (Scanner userObj = new Scanner(System.in)) {
-                System.out.println("Enter number of songs between 2 and 20");
-                userTracklist = userObj.nextInt();
-            }
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter number of songs between 2 and 20");
+            userTracklist = in.nextInt();
         }
-        while (userTracklist < 2 || userTracklist > 20);
+        while (userTracklist < min || userTracklist > max);
+        
 
-        HashMap<String, Integer> track = new HashMap<String, Integer>();
+        ArrayList<String> trackList = new ArrayList<String>();
+        ArrayList<Integer> durationList = new ArrayList<Integer>();
+        
+        for (int i = 1; i < userTracklist; i++ ){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Title of track :" + i);
+        String track = scan.next();
+        System.out.println("You entered: " + track);
+        trackList.add(track);
+        
+
+        Scanner seconds = new Scanner(System.in);
+        System.out.println("Duration of track " + i + " in seconds :");
+        int duration = seconds.nextInt();
+        durationList.add(duration);
+        System.out.println("You entered: " + duration + " in seconds");
+        
+        }
     
-        track.put("Stairways to heaven", 479);
-        System.out.println(track);     
+
+           System.out.println(trackList);
+           System.out.println(durationList);
     }    
 }
