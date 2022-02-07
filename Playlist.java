a516abc36c48f078fb907d1ffac01c23f1066b1d
MWest2020 <mwesterweel@hotmail.com>
2022-02-07 01:04:25 +0100

    user input for track number

--- Playlist.java
+++ Playlist.java
@@ -1,7 +1,43 @@
 import java.util.Scanner;
+import java.util.HashMap;
+
+/*
+*     @author Mark Westerweel, Studentnumber 500836508
+*
+*   The goal of this program is to ask the use for an X amount of tracks to make a tracklist.
+*   User is then asked for name and length of said tracks.
+*   The following is printed after user has given the data:
+*   - titles of tracks
+*   - title and lengths of tracks
+*   - total length of playlist and longest track
+*   - and again the entire playlist with titles and length per standards HvA  
+*/
+
 
 class Playlist {
-    public static void main(String [] args){
+     
+    public static int acceptTracklist(){
+
+        try (Scanner userObj = new Scanner(System.in)) {
+            System.out.println("Enter number of songs");
+            int userTracklist = userObj.nextInt();
+
+            if ( userTracklist >= 2 && userTracklist<= 20 ){
+                System.out.println("User tracklist is " + userTracklist+ " long");
+            } else {
+                System.out.println("User tracklist must be more than one and less than 21 tracks");
+                System.out.println("Enter number of songs");
+                userTracklist = userObj.nextInt();
+                acceptTracklist();
+            }
+            return userTracklist;
+        }
+        
+    }
+
+
+    
+    public static void main(String[] args){
         String firstName = "Mark";
         String lastName = "Westerweel";
         String studentNumber = "500836508";
@@ -10,6 +46,13 @@ class Playlist {
         System.out.println("Last name is: " + lastName);
         System.out.println("Student number is: " + studentNumber);
 
-        Scanner 
-    }
+        acceptTracklist();   
+
+        
+
+        // HashMap<String, Integer> track = new HashMap<String, Integer>();
+    
+        // track.put("Stairways to heaven", 479);
+        // System.out.println(track);     
+    }    
 }
